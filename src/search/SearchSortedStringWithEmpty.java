81a4b31b0b47586fb092659e2daae64ee55d0b80
baojialiang <504824862@qq.com>
2013-07-11 05:39:21 -0400

    search string in an array with empty string in it (only created the framework, this should use recursion)

---
+++ src/search/SearchSortedStringWithEmpty.java
@@ -0,0 +1,17 @@
+package search;
+
+public class SearchSortedStringWithEmpty {
+
+
+	public static void main(String[] args) {
+
+
+	}
+	
+	public static int searchString(int[] data, int value, int i, int j){
+		if(data[i] == data[j] && data[i] == value)
+			return i;
+		
+	}
+
+}
