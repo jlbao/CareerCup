6cbe58b3e242148fffbeb70cd6ea5a27084e247d
baojialiang <504824862@qq.com>
2013-07-12 08:00:36 -0400

    search sorted string with empaty strings mixing

--- src/search/FindElementInOneHalfSortedArray.java
+++ src/search/FindElementInOneHalfSortedArray.java
@@ -5,13 +5,50 @@ public class FindElementInOneHalfSortedArray {
 	public static void main(String[] args) {
 		int[] data = {6,8,35,39,1,2,3,4,5};
 		System.out.println(getElementTurnPoint(data));
-		System.out.println(findElement(data,3));
+		System.out.println(findElement(data,35));
 	}
 	
-	public static int findElement(int[] data, int value){
-		int i = getElementTurnPoint(data);
+	public static int find(int[] data, int value){
+		int i = 0;
 		int j = data.length - 1;
-		while(i < j){
+		while(i <= j){
+			int mid = (i + j) / 2;
+			if(value == data[mid]){
+				return mid;
+			}else if(data[i] < data[mid]){
+				if(value < data[i])
+					i = mid + 1;
+				else
+					j = mid - 1;
+			}else{ //data[i] > data[mid]
+				if(value < data[mid])
+					j = mid - 1;
+					
+			}
+			
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	public static int findElement(int[] data, int value){
+		int turnPoint = getElementTurnPoint(data);
+		int i, j;
+		if(data[turnPoint] == value)
+			return turnPoint; 
+		else if(value < data[turnPoint - 1]){
+			i = 0;
+			j = turnPoint - 1;
+		}else{
+			i = turnPoint;
+			j = data.length - 1;
+		}
+
+		while(i <= j){
 			int mid = (i + j) / 2;
 			if(data[mid] == value)
 				return mid;
--- src/search/SearchSortedStringWithEmpty.java
+++ src/search/SearchSortedStringWithEmpty.java
@@ -4,14 +4,48 @@ public class SearchSortedStringWithEmpty {
 
 
 	public static void main(String[] args) {
-
-
+		String[] data = {"abc","", "", "bb", "cc", "", "dd"};
+		System.out.println(searchString(data, "dd", 0, data.length - 1));
 	}
 	
-	public static int searchString(int[] data, int value, int i, int j){
-		if(data[i] == data[j] && data[i] == value)
-			return i;
+	public static int searchString(String[] data, String value, int i, int j){
+
+		while(i <= j){
+			int mid = (i + j) / 2;
+			if(data[mid] == ""){
+				int left = searchString(data, value, mid + 1, j);
+				int right = searchString(data, value, i, mid - 1);
+				
+				if(left != -1)
+					return left;
+				else if(right != -1)
+					return right;
+				else
+					return -1;
+			}else if(value == data[mid])
+				return mid;
+			else if(isBigger(value,data[i]))
+				i = mid + 1;
+			else
+				j = mid - 1;
+		}
+		
+		return -1;
 		
 	}
+	
+	public static boolean isBigger(String data1, String data2){
+		int i = 0;
+		while(i < data1.length() && i < data2.length()){
+			if((int)data1.charAt(i) < (int)data2.charAt(i))
+				return false;
+			else if((int)data1.charAt(i) > (int)data2.charAt(i))
+				return true;
+			else
+				i++;
+		}
+		return data1.length() > data2.length();
+	}
+	
 
 }
